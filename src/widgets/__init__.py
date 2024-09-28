8df9c04e1049e38f01558ae7c1d2b6e15d3031b4
Imolia Media <125727294+imoliamedia@users.noreply.github.com>
2024-09-28 08:00:33 +0200

    Delete src/widgets directory
    directory moved to root Widgets

--- src/widgets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .clock_widget import ClockWidget
-from .system_monitor_widget import SystemMonitorWidget
-# Import any other widgets you have
-
-__all__ = ['ClockWidget', 'SystemMonitorWidget']  # Add other widget names to this list
